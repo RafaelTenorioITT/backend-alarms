32b4b2ea3f8ed494ae4a4362032c533da56d42ba
Rafael Tenorio <rafaeltenorio897@gmail.com>
2026-01-17 03:03:59 -0800

    Fix Post

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
-  family: 4   // 👈 fuerza IPv4 (CLAVE)
+  family: 4
 });
 
 pool
